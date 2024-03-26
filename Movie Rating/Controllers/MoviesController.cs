eddbe2ecdadbec11bf6b34a9a7ba71bf860b851a
ckontos <har.kontos@hotmail.com>
2024-03-27 00:46:50 +0200

    Add project files.

---
+++ Movie Rating/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Rating.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public MoviesController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return new[]
+            {
+                new Movie {Id = 1, Title = "The Godfather", Year = 1972},
+                new Movie {Id = 2, Title = "Schindler's List", Year = 1993},
+                new Movie {Id = 3, Title = "WALL·E", Year = 2008}
+            };
+        }
+    }
+}
---
+++ Movie Rating/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Rating
+{
+    public class Movie
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int? Year { get; set; }
+    }
+}
