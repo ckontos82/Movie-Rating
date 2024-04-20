91486dd1cfc9f4e61a0c5ebe4037b25c8bd0b5bd
ckontos <har.kontos@hotmail.com>
2024-04-21 02:55:22 +0300

    Added post method in Movies Controller.

--- Movie Rating/Controllers/MoviesController.cs
+++ Movie Rating/Controllers/MoviesController.cs
@@ -9,6 +9,17 @@ namespace Movie_Rating.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
+
+        //For demonstration/educational purposes.
+        //This private List will be removed and replaced with db support.
+        private static List<Movie> _movies = new List<Movie>
+        {
+            new Movie { Id = 1, Title = "The Godfather", Year = 1972 },
+            new Movie { Id = 2, Title = "Schindler's List", Year = 1993 },
+            new Movie { Id = 3, Title = "WALL·E", Year = 2008 },
+            new Movie { Id = 4, Title = "The Matrix", Year = 1999 }
+        };
+
         private ILogger<MoviesController> _logger;
 
         public MoviesController(ILogger<MoviesController> logger)
@@ -16,18 +27,21 @@ namespace Movie_Rating.Controllers
             _logger = logger;
         }
 
+        [HttpGet("getbyid/{id}")]
+        public ActionResult<Movie> GetById(int id)
+        {
+            var movie = _movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return movie;
+        }
+
         [HttpGet("getmovies")]
         public ActionResult<IEnumerable<Movie>> Get()
         {
-            var movies = new List<Movie>
-            {
-                new Movie {Id = 1, Title = "The Godfather", Year = 1972},
-                new Movie {Id = 2, Title = "Schindler's List", Year = 1993},
-                new Movie {Id = 3, Title = "WALL·E", Year = 2008},
-                new Movie {Id = 4, Title = "The Matrix", Year = 1999}
-            };
-
-            var validationResults = movies.Select(movie =>
+            var validationResults = _movies.Select(movie =>
                 {
                     var context = new ValidationContext(movie);
                     var results = new List<ValidationResult>();
@@ -39,7 +53,24 @@ namespace Movie_Rating.Controllers
 
             return validationResults.Any()
                 ? BadRequest(validationResults.SelectMany(x => x.Results).ToList())
-                : Ok(movies);
+                : Ok(_movies);
+        }
+
+        [HttpPost("addmovie")]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            var context = new ValidationContext(movie);
+            var results = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(movie, context, results, true);
+
+            Func<Movie, IActionResult> addMovie = (m) =>
+            {
+                m.Id = _movies.Any() ? _movies.Max(x => x.Id) + 1 : 1;
+                _movies.Add(m);
+                return CreatedAtAction(nameof(GetById), new { id = m.Id }, m);
+            };
+
+            return isValid ? addMovie(movie) : BadRequest(results);
         }
     }
 }
